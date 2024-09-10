a31f56ebef7fc0d435bc00712703bc890e968aa4
JoshuaDick <Joshua.D.Dick-1@ou.edu>
2024-09-09 22:03:05 -0500

    fixed some comments

--- Log_RPM.py
+++ Log_RPM.py
@@ -7,7 +7,7 @@ import time
 
 csv_filename = 'RPM_log.csv' #name of file to log data into
 SCALE = 20.0 #Hz
-THRESHOLD = 5.0 #RISING EDGE THRESHOLD IN VOLTS
+THRESHOLD = 0.1 #RISING EDGE THRESHOLD IN VOLTS
 
 
 with open(csv_filename,mode='w',newline='') as csvfile:
@@ -19,15 +19,15 @@ with open(csv_filename,mode='w',newline='') as csvfile:
     with nidaqmx.Task() as task:
         #Define channel for NI 9205 (pins 1 & 19)
         task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai0",min_val=0,max_val=10)
-        #250Khz sample rate
-        task.timing.cfg_samp_clk_timing(100000,sample_mode=AcquisitionType.CONTINUOUS,samps_per_chan=100)
+        #100Khz sample rate
+        task.timing.cfg_samp_clk_timing(100000,sample_mode=AcquisitionType.CONTINUOUS,samps_per_chan=1000)
 
         print("Logging RPM...")
         task.start()
         delay_start = time.time()
         DataPoints = []
         while True:
-                #Read in an array of 100 samples at 100kHz
+                #Read in an array of 1000 samples at 100kHz
                 Vin = task.read(number_of_samples_per_channel = nidaqmx.constants.READ_ALL_AVAILABLE)
                 if len(Vin) > 0:
                 #Average the number of consecutive high signals in the array
@@ -46,7 +46,8 @@ with open(csv_filename,mode='w',newline='') as csvfile:
 
                          highTime = avgConsecutiveHighs/100000
                         #Equation for rpm from duty cycle and measured high time
-                         rpm = 0.5/(highTime)*60/SCALE
+                         rpm = 0.499/(highTime)*60/SCALE
+                         
                          #LIMIT TO 10HZ TO PREVENT INSANE STORAGE OVERFLOW
                          DataPoints.append(rpm)
                          if time.time() - delay_start > 0.1:
