d6a0172735f6afedd8b7051e4ec860620c4f7d19
JoshuaDick <Joshua.D.Dick-1@ou.edu>
2024-09-14 12:17:04 -0500

    changes for consistency

--- data-logging/Log_Force.py
+++ data-logging/Log_Force.py
@@ -20,35 +20,35 @@ with open(csv_filename,mode='w',newline='') as csvfile:
         task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai0",min_val=-0.1,max_val=0.1)
     #read samples continuously
     
-        task.timing.cfg_samp_clk_timing(1000.0)
-        task.ai_channels.ai_adc_timing_mode=nidaqmx.constants.ADCTimingMode.HIGH_RESOLUTION
+        task.timing.cfg_samp_clk_timing(50,sample_mode=AcquisitionType.CONTINUOUS,samps_per_chan=2)
+        task.ai_channels.ai_adc_timing_mode=nidaqmx.constants.ADCTimingMode.HIGH_SPEED
         
         #Measured voltage from the supply to the Load Cell
-        Voltage = 12.03
+        Voltage = 12.04
+        '''
         while rt != 'y':
             rt = input('ready to Tare? y/n')
         Zero = 0
         for i in range(1000):
             Zero += task.read()
+        '''
 
+        Zero = 0.3785
         slope = 1000/(float(Voltage)*2)
         offset = slope*Zero
 
+        task.start()
         print("Logging Torque...")
-        delay_start = time.time()
         while True:
-            Vin = task.read()*1000
-            Force = Vin*slope-offset
-            #print(Vin, " mV")
-            #print("Force = ", Force, " LB")
+            Vin = task.read(number_of_samples_per_channel = nidaqmx.constants.READ_ALL_AVAILABLE)*1000
+            if (len(Vin) > 0):
+                avgVin = sum(Vin)/len(Vin)
+                Force = avgVin*slope-offset
 
-            #LIMIT TO 10HZ FOR STORAGE
-            if (time.time() - delay_start > 0.1):
-                delay_start = time.time()
                 #Get formatted timestamp
                 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
-                writer.writerow({'Timestamp': timestamp, 'Torque (Ft-LB)': Force, 'Voltage (mV)': Vin})
+                writer.writerow({'Timestamp': timestamp, 'Torque (Ft-LB)': Force, 'Voltage (mV)': avgVin})
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #This will turn into the primary executable
+
+import threading
+import subprocess
+
+def runTorque():
+    subprocess.run(["python","data-logging\Log_Force.py"])
+
+def runRPM():
+    subprocess.run(["python","data-logging\Log_RPM.py"])
+
+
+if __name__ == "__main__":
+    thread1 = threading.Thread(target=runTorque)
+    thread2 = threading.Thread(target=runRPM)
+    
+    thread1.start()
+    print("Torque Running")
+    thread2.start()
+    print("RPM Running")
+
+    thread1.join()
+    thread2.join()
