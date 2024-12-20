b0bb9211f5786411d578a2206adf50d3b3a6b50b
Josh <98294457+JoshuaDick@users.noreply.github.com>
2024-12-19 19:57:49 -0600

    Quality of life changes

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from tkinter import Label
 import os
 
 def dark_title_bar(window):
-    #print(platform.platform())
     if 'Windows' in platform.platform():
         window.update()
         set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
@@ -23,21 +22,25 @@ def dark_title_bar(window):
 
 def run_process(process_number):
     new_window = tk.Toplevel(root)
+    new_window.config(bg='black')
+    new_window.resizable(False,False)
+    new_window.attributes('-topmost',True)
+    dark_title_bar(new_window)
     root.withdraw()
 
         # Add a button to return to the main window
     new_window.protocol("WM_DELETE_WINDOW", lambda:doNothing())
-    back_button = tk.Button(new_window, text="Back to Main", command=lambda:returnToMain(process,new_window))
+    back_button = tk.Button(new_window, text="Back to Main", command=lambda:returnToMain(process,new_window),bg='black',fg='white',activebackground='navy',activeforeground='white')
     back_button.pack(pady=5)
     # Instead of running the subprocess directly, open a new window
     if process_number == 1:
         new_window.title("Live RPM & Torque")
-        label = tk.Label(new_window, text="Running Live RPM & Torque...")
+        label = tk.Label(new_window, text="Running Live RPM & Torque...", bg='black',fg='white')
         label.pack(pady=10)
         process = subprocess.Popen(['python',r'dyno-interface\\Quick&Dirty\\LivePlotter.py'])  # Simulate running a process
     else:
         new_window.title("Recording Interface")
-        label = tk.Label(new_window, text="Running Recording Interface...")
+        label = tk.Label(new_window, text="Running Recording Interface...",bg='black',fg='white')
         label.pack(pady=10)
         process = subprocess.Popen(["python",r'dyno-interface\\dyno-interface\\app.py'])  # Simulate running a process
         webbrowser.open("http://127.0.0.1:8050/")
@@ -53,9 +56,7 @@ def kill_subprocesses(parent_pid):
         # Get all children of the parent process
         children = parent.children(recursive=True)
         for child in children:
-            #print(f"Killing subprocess: {child.pid} ({child.name()})")
-            child.kill()  # or child.terminate() for a graceful exit
-        #print(f"Killed all subprocesses of parent PID {parent_pid}")
+            child.kill() 
     except psutil.NoSuchProcess:
         print(f"No process with PID {parent_pid} found.")
     except Exception as e:
@@ -105,10 +106,10 @@ root.geometry("500x500")
 label = tk.Label(root, text="Welcome to the Dyno Interface.",bg='black',fg='white',font=('Times',25))
 label.pack(pady=(0,25))
 # Create buttons for selection
-live_rpm_button = tk.Button(root, text="Live RPM & Torque", command=on_live_rpm, bg='gray9',fg='white',font='Times')
-recording_button = tk.Button(root, text="Launch Recording Interface", command=on_recording_interface, bg='gray9',fg='white',font='Times')
+live_rpm_button = tk.Button(root, text="Live RPM & Torque", command=on_live_rpm, bg='gray9',fg='white',font='Times',activebackground='navy',activeforeground='white')
+recording_button = tk.Button(root, text="Launch Recording Interface", command=on_recording_interface, bg='gray9',fg='white',font='Times',activebackground='navy',activeforeground='white')
 live_rpm_button.pack(pady=5)
-recording_button.pack(pady=5)
+recording_button.pack(pady=20)
 root.resizable(False,False)
 dark_title_bar(root)
 root.after(0, update, 0)
