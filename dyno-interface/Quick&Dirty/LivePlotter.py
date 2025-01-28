c126c7c2e5d929448640bbc4ab2a11a9c761942e
JoshuaDick <Joshua.D.Dick-1@ou.edu>
2025-01-28 17:41:16 -0600

    HP to plot list

--- dyno-interface/Quick&Dirty/LivePlotter.py
+++ dyno-interface/Quick&Dirty/LivePlotter.py
@@ -6,13 +6,18 @@ def plotRPM():
     subprocess.run(['python',r'dyno-interface\\Quick&Dirty\\liveRPM.py'])
 def plotTorque():
     subprocess.run(['python',r'dyno-interface\\Quick&Dirty\\liveTorque.py'])
+def plotHP():
+    subprocess.run(['python',r'dyno-interface\\Quick&Dirty\\liveTorque.py'])
 
 
 if __name__ == "__main__":
     rpm = threading.Thread(target=plotRPM)
     torque = threading.Thread(target=plotTorque)
+    hp = threading.Thread(target=plotHP)
+    hp.start()
     rpm.start()
     torque.start()
+    
 
 
     
